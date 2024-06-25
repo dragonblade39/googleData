7553063677caa0c817164035fcd83226bf7bb2a7
Sadineni-Tejaswi <sadinenitejaswi23.csedvit@gmail.com>
2024-06-25 23:27:30 +0530

    main

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const FL = require("./routes/userRoutes");
+const OL = require("./routes/otpRoutes");
 const cors = require("cors");
 
 const app = express();
@@ -11,7 +12,7 @@ mongoose.connect(
 );
 const db = mongoose.connection;
 db.on("open", () => {
-  console.log("Database Connected");
+  // console.log("Database Connected");
 });
 db.on("error", () => {
   console.log("Database not Connected");
@@ -20,8 +21,9 @@ db.on("error", () => {
 app.use(express.json());
 app.use(cors());
 app.use("/User-Data", FL);
+app.use("/Otp-Data", OL);
 
 const port = 5500;
 app.listen(port, () => {
-  console.log("Server Started on " + port);
+  // console.log("Server Started on " + port);
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const Schema = require("../schema/schema");
+const otpSchema = require("../schema/otpSchema");
 const bcrypt = require("bcrypt");
 require("dotenv").config();
 const nodemailer = require("nodemailer");
+const axios = require("axios");
 
 let transporter = nodemailer.createTransport({
   service: "gmail",
@@ -14,133 +16,24 @@ let transporter = nodemailer.createTransport({
   debug: true,
 });
 
-transporter.verify((error, success) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Ready for messages");
-    console.log(success);
-  }
-});
-
-router.post("/otp", async (req, res, next) => {
-  try {
-    const { otp, email } = req.body;
-    const mailOptions = {
-      from: `"Chethan N V" <${process.env.AUTH_EMAIL}>`,
-      to: email,
-      subject: "Verification Code for Account Activation!!!",
-      html: `
-        <p>Dear User,</p>
-        <p>Please use the following verification code to activate your account:</p>
-        <p><b>${otp}</b></p>
-        <br />
-        <p>Best regards,</p>
-        <p>Chethan N V</p>
-      `,
-    };
-    await transporter.sendMail(mailOptions);
-    return res.status(200).json("Successfull");
-  } catch (error) {
-    res.json({
-      status: "FAILED",
-      message: error.message,
-    });
-  }
-});
-
-router.post("/welcome", async (req, res, next) => {
-  try {
-    const { email } = req.body;
-    const existingEmail = await Schema.findOne({ email });
-    const name = existingEmail.name;
-    const mailOptions = {
-      from: `"Chethan N V" <${process.env.AUTH_EMAIL}>`,
-      to: email,
-      subject: "Welcome to OurPlatform!!!",
-      html: `
-        <p>Hello ${name},</p>
-        <p>Welcome to OurPlatform! We're excited to have you on board!!!</p>
-        <br />
-        <p>Best regards,</p>
-        <p>Chethan N V</p>
-      `,
-    };
-    await transporter.sendMail(mailOptions);
-    return res.status(200).json("Successfull");
-  } catch (error) {
-    res.json({
-      status: "FAILED",
-      message: error.message,
-    });
-  }
-});
-
-router.post("/forgotpasswordemail", async (req, res, next) => {
-  try {
-    const { email, otp } = req.body;
-    const existingEmail = await Schema.findOne({ email });
-    const name = existingEmail.name;
-    const mailOptions = {
-      from: `"Chethan N V" <${process.env.AUTH_EMAIL}>`,
-      to: email,
-      subject: "Password Change Requested!!!",
-      html: `
-        <p>Hello ${name},</p>
-        <p>Please use the following verification code to change your password:</p>
-        <p><b>${otp}</b></p>
-        <br />
-        <p>Best regards,</p>
-        <p>Chethan N V</p>
-      `,
-    };
-    await transporter.sendMail(mailOptions);
-    return res.status(200).json("Successfull");
-  } catch (error) {
-    res.json({
-      status: "FAILED",
-      message: error.message,
-    });
-  }
-});
-
-router.post("/changedpassword", async (req, res, next) => {
-  try {
-    const { email } = req.body;
-    const existingEmail = await Schema.findOne({ email });
-    const name = existingEmail.name;
-    const mailOptions = {
-      from: `"Chethan N V" <${process.env.AUTH_EMAIL}>`,
-      to: email,
-      subject: "Password Changed!!!",
-      html: `
-        <p>Hello ${name},</p>
-        <p>Password has been updated successfully!!!</p>
-        <br />
-        <p>Best regards,</p>
-        <p>Chethan N V</p>
-      `,
-    };
-    await transporter.sendMail(mailOptions);
-    return res.status(200).json("Successfull");
-  } catch (error) {
-    res.json({
-      status: "FAILED",
-      message: error.message,
-    });
-  }
-});
+// transporter.verify((error, success) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("Ready for messages");
+//     console.log(success);
+//   }
+// });
 
 router.post("/create", async (req, res, next) => {
-  const { name, email, password, verified } = req.body;
-  const existingEmail = await Schema.findOne({ email: email });
-
-  if (existingEmail) {
-    return res.status(400).json("Email already exists.");
-  }
-  const saltRounds = 10;
-  const hashedPassword = await bcrypt.hash(password, saltRounds);
   try {
+    const { name, email, password, verified } = req.body;
+    const existingEmail = await Schema.findOne({ email: email });
+    if (existingEmail) {
+      return res.status(400).json("Email already exists.");
+    }
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
     const newUser = new Schema({
       name,
       email,
@@ -148,8 +41,18 @@ router.post("/create", async (req, res, next) => {
       verified: verified,
     });
     await newUser.save();
+    //await axios.post("http://localhost:5500/Otp-Data/otpstore", {
+    await axios.post(
+      "https://googledata-backend.onrender.com/Otp-Data/otpstore",
+      {
+        email: email,
+        name: name,
+      }
+    );
+
     return res.status(200).json("Successfull");
   } catch (error) {
+    console.error("Error:", error);
     next(error);
   }
 });
@@ -158,7 +61,7 @@ router.post("/data", (req, res, next) => {
   const { email } = req.body;
   Schema.findOne({ email: email }).then((login) => {
     if (login) {
-      return res.json(login);
+      return res.status(200).json(login);
     } else {
       return res.status(400).json("No record exits");
     }
@@ -196,16 +99,48 @@ router.post("/login", async (req, res, next) => {
   }
 });
 
+const welcome = async (email, name) => {
+  const mailOptions = {
+    from: `"Chethan N V" <${process.env.AUTH_EMAIL}>`,
+    to: email,
+    subject: "Welcome to OurPlatform!!!",
+    html: `
+        <p>Hello ${name},</p>
+        <p>Welcome to OurPlatform! We're excited to have you on board!!!</p>
+        <br />
+        <p>Best regards,</p>
+        <p>Chethan N V</p>
+      `,
+  };
+  await transporter.sendMail(mailOptions);
+};
+
 router.post("/update", async (req, res) => {
   try {
-    const { email } = req.body;
+    const { email, otp } = req.body;
     const existingUser = await Schema.findOne({ email });
 
     if (existingUser) {
-      existingUser.verified = true;
-      await existingUser.save();
-
-      res.status(200).json({ message: "User data updated successfully" });
+      if (existingUser.verified === true) {
+        res.status(200).json({ message: "User already verified!" });
+      } else {
+        const response = await axios.post(
+          // "http://localhost:5500/Otp-Data/otpcheck",
+          "https://googledata-backend.onrender.com/Otp-Data/otpcheck",
+          {
+            email: email,
+            otp: otp,
+          }
+        );
+        if (response.status === 200) {
+          existingUser.verified = true;
+          await existingUser.save();
+          await welcome(email, existingUser.name);
+        } else {
+          return existingUser.message;
+        }
+      }
+      res.status(200).json({ message: "User verified successfully" });
     } else {
       res.status(404).json({ message: "User not found" });
     }
@@ -213,6 +148,22 @@ router.post("/update", async (req, res) => {
     res.status(400).json({ message: "Failed to save/update user data" });
   }
 });
+
+const passwordChanged = async (email, name) => {
+  const mailOptions = {
+    from: `"Chethan N V" <${process.env.AUTH_EMAIL}>`,
+    to: email,
+    subject: "Password Changed!!!",
+    html: `
+        <p>Hello ${name},</p>
+        <p>Password has been updated successfully!!!</p>
+        <br />
+        <p>Best regards,</p>
+        <p>Chethan N V</p>
+      `,
+  };
+  await transporter.sendMail(mailOptions);
+};
 
 router.post("/updatePassword", async (req, res) => {
   try {
@@ -223,8 +174,8 @@ router.post("/updatePassword", async (req, res) => {
       const hashedPassword = await bcrypt.hash(password, 10);
       existingUser.password = hashedPassword;
       await existingUser.save();
-
       res.status(200).json({ message: "Password updated successfully" });
+      await passwordChanged(email, existingUser.name);
     } else {
       res.status(404).json({ message: "User not found" });
     }
@@ -232,4 +183,88 @@ router.post("/updatePassword", async (req, res) => {
     res.status(400).json({ message: "Failed to update password" });
   }
 });
+
+router.post("/otpForPasswordUpdate", async (req, res) => {
+  try {
+    const { email } = req.body;
+    const existingUser = await Schema.findOne({ email });
+    if (existingUser) {
+      const name = await existingUser.name;
+      const response = await axios.post(
+        // "http://localhost:5500/Otp-Data/otpreset",
+        "https://googledata-backend.onrender.com/Otp-Data/otpreset",
+        {
+          email: email,
+          name: name,
+        }
+      );
+      if (response.status === 200) {
+        existingUser.verified = false;
+        existingUser.save();
+        res.status(200).json({ message: "Otp sent successfully!!!" });
+      } else {
+        res.status(404).json({ message: response.message });
+      }
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: "Failed to send OTP" });
+  }
+});
+
+router.post("/verifyForPasswordUpdate", async (req, res) => {
+  const { email, otp } = req.body;
+
+  try {
+    const existingUser = await Schema.findOne({ email });
+
+    if (existingUser) {
+      if (existingUser.verified === true) {
+        return res.status(200).json({ message: "User already verified!" });
+      } else {
+        try {
+          const response = await axios.post(
+            //"http://localhost:5500/Otp-Data/otpcheck",
+            "https://googledata-backend.onrender.com/Otp-Data/otpcheck",
+            {
+              email: email,
+              otp: otp,
+            }
+          );
+
+          if (response.status === 200) {
+            existingUser.verified = true;
+            await existingUser.save();
+            return res
+              .status(200)
+              .json({ message: "User verified successfully" });
+          } else {
+            // Handle specific cases or default to an error message
+            return res.status(400).json({
+              message: response.data.message || "OTP verification failed",
+            });
+          }
+        } catch (error) {
+          // Log detailed error information
+          console.error(
+            "Error during OTP verification:",
+            error.response ? error.response.data : error.message
+          );
+          return res.status(400).json({
+            message: error.response
+              ? error.response.data
+              : "OTP verification service error",
+          });
+        }
+      }
+    } else {
+      return res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    console.error("Error finding user:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 module.exports = router;
